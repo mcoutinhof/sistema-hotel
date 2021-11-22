f1b4ae3f932abaa63143a7fe35039fd1f8f14320
Mateus Coutinho <maateus.coutinho@gmail.com>
2021-11-22 16:01:45 -0300

    Adaptei o lcurses.c para Windows.

--- lcurses.c
+++ lcurses.c
@@ -1,6 +1,5 @@
 #include "lcurses.h"
 #include <stdio.h>
-#include <termios.h>
 #include <unistd.h>
 
 void clrscr() {
@@ -15,6 +14,13 @@ void clrtoeol() {
     fputs("\033[0K", stdout);
 }
 
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+}
+
+#ifdef linux
+#include <termios.h>
+
 int getch() {
     struct termios old_cfg;
     tcgetattr(STDIN_FILENO, &old_cfg);
@@ -24,10 +30,6 @@ int getch() {
     register int ch = getchar();
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_cfg);
     return ch;
-}
-
-void gotoxy(int x, int y) {
-    printf("\033[%d;%dH", y, x);
 }
 
 void wherexy(int *x, int *y) {
@@ -40,3 +42,11 @@ void wherexy(int *x, int *y) {
     scanf("%*c%*c%d%*c%d%*c", y, x);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_cfg);
 }
+#elifdef _WIN32
+#include <conio.h>
+
+void wherexy(int *x, int *y) {
+    *x = wherex();
+    *y = wherey();
+}
+#endif
