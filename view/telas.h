e337470829c5d9de1f9cd8cc8cbbab4a8402ff8d
anewere <umarthurqualquer74@gmail.com>
2021-11-22 21:10:01 -0300

    ORDENACAO MELHORADA

--- view/telas.h
+++ view/telas.h
@@ -6,21 +6,6 @@ int menu_principal();
 // principal -> cadastros
 int menu_cadastros();
 
-// principal -> cadastros -> hospedes
-int menu_hospedes();
-
-// principal -> cadastro -> hospede -> cadastrar_hospede
-int cadastrar_hospede();
-
-// principal -> cadastro -> hospede -> ver_hospede
-int ver_hospede();
-
-int menu_hotel();
-int menu_categoria();
-int menu_acomodacao();
-
-//principal -> cadastro ->
-
 /*ORDEM 
 Hospedes --- e logo em seguida tudo relacionado com ele
 hotel
@@ -30,3 +15,47 @@ Acomodações
 Produtos
 Fornecedores
 */
+
+//###HOSPEDE
+
+// principal -> cadastros -> hospedes
+int menu_hospedes();
+
+// principal -> cadastro -> hospede -> cadastrar_hospede
+int cadastrar_hospede();
+
+// principal -> cadastro -> hospede -> ver_hospede
+int ver_hospede();
+
+//###HOTEL
+
+// principal -> cadastros -> hotel
+int menu_hotel();
+
+// principal -> cadastro -> hotel -> cadastrar_hotel
+int cadastrar_hotel();
+
+// principal -> cadastro -> hotel -> ver_hotel
+//int ver_hotel();
+
+//###CATEGORIA
+
+// principal -> cadastros -> categoria
+int menu_categoria();
+
+// principal -> cadastro -> categoria -> cadastrar_categoria
+int cadastrar_categoria();
+
+// principal -> cadastro -> categoria -> ver_categoria
+//int ver_categoria();
+
+//###ACOMODACAO
+
+// principal -> cadastros -> acomodacao
+int menu_acomodacao();
+
+// principal -> cadastro -> acomodacao -> cadastrar_acomodacao
+int cadastrar_acomodacao();
+
+// principal -> cadastro -> acomodacao -> ver_acomodacao
+//int ver_acomodacao();
