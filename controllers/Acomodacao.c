e976a1c53a0da37a758dbf95494179111eff53eb
= <ryanfonseca183@gmail.com>
2022-01-06 11:35:54 -0300

    Criando arquivos de controle para as entidades do sistema

---
+++ controllers/Acomodacao.c
@@ -0,0 +1,43 @@
+#include "../lcurses.h"
+#include "../model/tables.h"
+#include "utils.h"
+#include <stdlib.h>
+#include "telas.h"
+
+int cadastrar_acomodacao() {
+    clrscr();
+
+    struct Acomodacao acomo = {}; // Sempre coloque as chaves pra zerar os valores da struct
+    form(0, Acomodacoes, &acomo); // Modo 0 = Inserir
+
+    DATABASE->open(Acomodacoes);
+    DATABASE->insert(Acomodacoes, &acomo);
+    DATABASE->close(Acomodacoes);
+    return EXIT_SUCCESS;
+}
+
+int ver_acomodacao() {
+    DATABASE->open(Acomodacoes);
+
+    DATABASE_forEach(struct Acomodacao, acom, Acomodacoes) {
+        clrscr();
+        form(1, Acomodacoes, &acom);
+        gotoxy(3, wherey() + 2);
+        int option = menu(4, "Próximo", "Editar", "Deletar", "Sair");
+        switch (option) {
+        case 0:
+            continue;
+        case 1:
+            clrscr(); // Sempre limpar a tela ...
+            form(2, Acomodacoes, &acom);
+            DATABASE->update(Acomodacoes, &acom);
+            break;
+        case 2:
+            DATABASE->delete (Acomodacoes);
+            break;
+        }
+        if (option == 3)
+            break;
+    }
+    DATABASE->close(Acomodacoes);
+}
---
+++ controllers/Hotel.c
@@ -0,0 +1,44 @@
+#include "../lcurses.h"
+#include "../model/tables.h"
+#include "utils.h"
+#include <stdlib.h>
+#include "telas.h"
+
+int cadastrar_hotel() {
+    clrscr();
+
+    struct Hotel hotel = {}; // Sempre coloque as chaves pra zerar os valores da struct
+    form(0, Hoteis, &hotel); // Modo 0 = Inserir
+
+    DATABASE->open(Hoteis);
+    DATABASE->insert(Hoteis, &hotel); // Insere o hotel no banco de dados
+    DATABASE->close(Hoteis);
+    return EXIT_SUCCESS;
+}
+
+int ver_hotel() {
+    DATABASE->open(Hoteis);
+
+    DATABASE_forEach(struct Hotel, hotel, Hoteis) {
+        clrscr();
+        form(1, Hoteis, &hotel);
+        gotoxy(3, wherey() + 2);
+        int option = menu(4, "Próximo", "Editar", "Deletar", "Sair");
+        switch (option) {
+        case 0:
+            continue;
+        case 1:
+            clrscr();
+            form(2, Hoteis, &hotel);
+            DATABASE->update(Hoteis, &hotel);
+            break;
+        case 2:
+            DATABASE->delete (Hoteis);
+            break;
+        }
+        if (option == 3)
+            break;
+    }
+
+    DATABASE->close(Hoteis);
+}
